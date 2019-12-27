2a30f6305b7e4f01a95c071e5314e0fdf214df04
Markus Heldrup <markusheldrup@hotmail.com>
2019-12-27 22:56:53 +0100

    merge conflict in gameclass

---
+++ components/game/GameComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button, View, Text } from "react-native";
+
+const GameComponent = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+};
+
+export default GameComponent;
